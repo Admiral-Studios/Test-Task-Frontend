091095364843796a33117b0f043f419584ef24f3
Dima Cherednyk <dicherednyk@gmail.com>
2023-02-07 00:11:42 +0200

    Solution

--- development/js/script.js
+++ development/js/script.js
@@ -1,1 +1,13 @@
-//script
+'use strict';
+
+const optionsList = document.querySelector('.messages__optionList');
+
+document.addEventListener('click', e => {
+  if (e.target.closest('.messages__optionImage')) {
+    if (document.querySelector('.messages__optionList--opened')) {
+      optionsList.classList.remove(`messages__optionList--opened`);
+    } else {
+      optionsList.classList.add(`messages__optionList--opened`);
+    }
+  }
+});
